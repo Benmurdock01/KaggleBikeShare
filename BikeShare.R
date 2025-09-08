af7ef67251abd0818894330862f97a32332c4437
Ben Murdock <159577141+Benmurdock01@users.noreply.github.com>
2025-09-08 12:16:07 -0600

    initial eda
    downloaded the data, cleaned it up a bit and then started EDA on the 'train' data.

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,38 @@
+# install.packages('GGally')
+# install.packages('patchwork')
 library(tidyverse)
+library(ggplot2)
 library(tidymodels)
+library(vroom)
+library(dplyr)
+library(GGally)
+library(patchwork)
+
+Bike <- vroom("Git/KaggleBikeShare/train.csv")
+
+Bike <- Bike %>% 
+  mutate(
+    weather = as.factor(weather),
+    holiday = as.factor(holiday),
+    workingday = as.factor(workingday),
+    season = as.factor(season)
+  )
+
+Bike <- Bike %>%
+  mutate(season = factor(season,
+                         levels = c(1, 2, 3, 4),
+                         labels = c("Spring", "Summer", "Fall", "Winter")))
+
+plot1 <- ggplot(data = Bike, mapping = aes(x = season, y = count))+
+  geom_bar(stat = "summary", fun = "mean")
+
+plot2 <- ggplot(data = Bike, aes(x=weather,y = count))+
+  geom_bar(stat = "summary", fun = "mean")
+
+plot3 <- ggplot(data = Bike, aes(x=workingday, y = count))+
+  geom_bar(stat = 'summary', fun = 'mean')
+
+plot4 <- ggplot(data = Bike, aes(x=windspeed, y = count))+
+  geom_point()
+
+(plot1 + plot2)/(plot3 + plot4)
