a105ce70b6f2b63786cb7d0b2ed9fcdcfbf7023a
Ben Murdock <159577141+Benmurdock01@users.noreply.github.com>
2025-09-15 10:04:04 -0600

    2nd comp linear regression

--- BikeShare.R
+++ BikeShare.R
@@ -1,5 +1,3 @@
-# install.packages('GGally')
-# install.packages('patchwork')
 library(tidyverse)
 library(ggplot2)
 library(tidymodels)
@@ -7,7 +5,7 @@ library(vroom)
 library(dplyr)
 library(GGally)
 library(patchwork)
-library(tidymodels)
+# install.packages("workflows")
 
 BikeTrain <- vroom("Git/KaggleBikeShare/train.csv")
 
@@ -17,12 +15,41 @@ BikeTrain <- BikeTrain %>%
     holiday = as.factor(holiday),
     workingday = as.factor(workingday),
     season = as.factor(season)
-  )
+  ) %>% 
+  select(.,-casual) %>% 
+  select(.,-registered)
 
-BikeTrain <- BikeTrain %>%
-  mutate(season = factor(season,
-                         levels = c(1, 2, 3, 4),
-                         labels = c("Spring", "Summer", "Fall", "Winter")))
+BikeTest <- vroom("Git/KaggleBikeShare/test.csv") %>% 
+  mutate(
+    weather = as.factor(weather),
+    holiday = as.factor(holiday),
+    workingday = as.factor(workingday),
+    season = as.factor(season))
+
+
+#gernerating LM
+linear_m <- linear_reg() %>% 
+  set_engine('lm') %>% 
+  set_mode('regression') %>% 
+  fit(count~.-datetime, data=BikeTrain)
+
+#Generating prediction using lm
+bike_prediction <- predict(linear_m,
+                           new_data=BikeTest)
+bike_prediction
+
+#adjusting for kaggle
+kaggle_submission <- bike_prediction %>% 
+  bind_cols(.,BikeTest) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(count=pmax(0,count)) %>% 
+  mutate(datetime=as.character(format(datetime)))
+
+vroom_write(x=kaggle_submission, file = './LinearPreds.csv', delim = ',')
+
+
+#EDA plots
 Bike <- BikeTrain
 plot1 <- ggplot(data = Bike, mapping = aes(x = season, y = count))+
   geom_bar(stat = "summary", fun = "mean")
@@ -38,23 +65,50 @@ plot4 <- ggplot(data = Bike, aes(x=windspeed, y = count))+
 
 (plot1 + plot2)/(plot3 + plot4)
 
-#gernerating LM
-linear_m <- linear_reg() %>% 
-  set_engine('lm') %>% 
-  set_mode('regression') %>% 
-  fit(count~.-datetime, data=BikeTrain)
 
-#Generating prediction using lm
-bike_prediction <- predict(linear_m,
-                           new_data=Bike)
-bike_prediction
+#Homework recipe
 
-#adjusting for kaggle
-kaggle_submission <- bike_prediction %>% 
-  bind_cols(.,Bike) %>% 
-  select(datetime, .pred) %>% 
-  rename(count=.pred) %>% 
-  mutate(count=pmax(0,count)) %>% 
-  mutate(datetime=as.character(format(datetime)))
+# test data
+testData <- vroom("Git/KaggleBikeShare/test.csv")
 
-vroom_write(x=kaggle_submission, file = './LinearPreds.csv', delim = ',')
+# training data
+trainData <- vroom("Git/KaggleBikeShare/train.csv") %>%
+  select(-casual, -registered) %>%
+  mutate(count = log1p(count))   # log transform
+
+# recipe
+bike_recipe <- recipe(count ~ ., data = trainData) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
+  step_mutate(weather = factor(weather, levels = c(1,2,3))) %>% 
+  step_time(datetime, features = c("hour")) %>% 
+  step_mutate(season = factor(season, 
+                              levels = c(1,2,3,4),
+                              labels = c("spring","summer","fall","winter"))) %>% 
+  step_corr(all_numeric_predictors(), threshold = 0.5)
+prepped_recipe <- prep(bike_recipe)
+
+baked_train <- bake(prepped_recipe, new_data = trainData)
+baked_test <- bake(prepped_recipe, new_data = testData)
+
+# model
+bike.lm <- linear_reg() %>%
+  set_engine("lm") %>%
+  set_mode("regression")
+
+# workflow
+bike_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(bike.lm) %>%
+  fit(data = trainData)
+
+# predictions + backtransform
+lin_preds <- predict(bike_workflow, new_data = testData) %>%
+  mutate(count = pmax(0, round(expm1(.pred)))) %>%
+  bind_cols(testData %>% select(datetime))
+
+submission <- lin_preds %>%
+  select(datetime, count)
+
+vroom_write(x=submission, file = './LinearPreds2.csv', delim = ',')
+
+
